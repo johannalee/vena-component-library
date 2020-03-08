bd45f5c6155d7ee9e73797c8bc2c51e43fc1977d
Rebecca Song <rebecca.song@outlook.com>
2020-03-07 21:26:19 -0500

    Add banner and icons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,29 @@
-import { configure, addDecorator } from "@storybook/react";
-import { withA11y } from '@storybook/addon-a11y';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { create } from '@storybook/theming';
 
-addDecorator(withA11y);
+import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Theme
+const theme = create({
+  base: "light",
+  brandTitle: "Kardia by Vena",
+  brandImage: "/kardia-banner.png" 
+});
+
+// Parameters
+addParameters({
+  options: {
+    theme
+  }
+})
+
+// Decorators
+addDecorator(withA11y);
 
 configure(loadStories, module);
